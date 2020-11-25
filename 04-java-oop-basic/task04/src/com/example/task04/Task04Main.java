d6ebb8e9a737d1daf0fb8d0411c3c3c494191713
Марк <markshubat@gmail.com>
2020-11-25 14:19:26 +0500

    Шубат Задача 6.4

--- 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
+++ 04-java-oop-basic/task04/src/com/example/task04/Task04Main.java
@@ -4,4 +4,17 @@ public class Task04Main {
     public static void main(String[] args) {
 
     }
+    static String getSeason(int monthNumber) {
+        if (monthNumber > 12 || monthNumber < 1)
+            throw new UserException(
+                String.format("monthNumber %d is invalid, month number should be between 1..12", monthNumber));
+        if (monthNumber / 3 == 1)
+            return "весна";
+        else if (monthNumber / 3 == 2)
+            return "лето";
+        else if (monthNumber / 3 == 3)
+            return "осень";
+        else
+            return "зима";
+    }
 }
---
+++ 04-java-oop-basic/task04/src/com/example/task04/UserException.java
@@ -0,0 +1,8 @@
+package com.example.task04;
+
+public class UserException extends IllegalArgumentException {
+
+    public UserException(String message){
+        super(message);
+    }
+}
